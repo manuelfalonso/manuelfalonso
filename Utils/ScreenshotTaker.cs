4bc467048ee37e7a2290426b2365604217eaef4a
Manuel Francisco Alonso <manuelalonso89@gmail.com>
2021-08-17 16:40:26 -0300

    ScreenshotTaker utility script added

---
+++ Utils/ScreenshotTaker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Utility Class for taking a screenshot from the editor using a menu item.
+/// </summary>
+public class ScreenshotTaker : MonoBehaviour
+{
+    [MenuItem("Sombra Studios/Tools/Take Screenshot")]
+    public static void TakeScreenshot()
+    {
+        if (!Directory.Exists("Screenshots"))
+            Directory.CreateDirectory("Screenshots");
+
+        ScreenCapture.CaptureScreenshot(string.Format("Screenshots/{0}.png", 
+            DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss")));
+
+        Debug.Log("Screenshot saved at: " + Directory.GetCurrentDirectory() + "\\Screenshots");
+    }
+}
