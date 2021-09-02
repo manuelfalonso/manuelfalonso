f0bf8892791b79dcdda52a5732d38391b7af38c4
Manuel Francisco Alonso <manuelalonso89@gmail.com>
2021-09-02 18:06:59 -0300

    Advertisement scripts added

---
+++ Utils/Advertisement/AdsInitializer.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+/// <summary>
+/// Class for initializing the Advertisement API.
+/// It's recommended to run it as soon the application start running.
+/// It's needed the Android and/or IOS Game Id obtained from:
+/// Project Settings -> Services -> Ads (after activating Ads in the project)
+/// </summary>
+public class AdsInitializer : MonoBehaviour, IUnityAdsInitializationListener
+{
+    [SerializeField] string _androidGameId;
+    [SerializeField] string _iOsGameId;
+    [SerializeField] bool _testMode = true;
+    [SerializeField] bool _enablePerPlacementMode = true;
+    private string _gameId;
+
+    void Awake()
+    {
+        InitializeAds();
+    }
+
+    public void InitializeAds()
+    {
+        _gameId = (Application.platform == RuntimePlatform.IPhonePlayer)
+            ? _iOsGameId
+            : _androidGameId;
+        Advertisement.Initialize(_gameId, _testMode, _enablePerPlacementMode, this);
+    }
+
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialization complete.");
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+    }
+}
