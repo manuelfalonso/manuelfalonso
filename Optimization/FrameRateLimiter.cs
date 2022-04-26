ad3dd5ebb833c81b77f5c01e6231eee5217f1bd9
Manuel Francisco Alonso <manuelalonso89@gmail.com>
2022-04-26 12:04:49 -0300

    New FPS Limiter script

---
+++ Optimization/FrameRateLimiter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+/// <summary>
+/// Limit FPS to optimize performance to the maximum refresh rate of the monitor
+/// This script don't need to and can't be attached to any Game Object
+/// </summary>
+public static class FrameRateLimiter
+{
+    [RuntimeInitializeOnLoadMethod]
+    public static void LimitFrameRate()
+    {
+        Application.targetFrameRate = Screen.currentResolution.refreshRate;
+    }
+}
