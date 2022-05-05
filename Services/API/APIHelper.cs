d8acf2ec08dd2dcf4a6431d822d1915d44bbd572
Manuel Francisco Alonso <manuelalonso89@gmail.com>
2022-05-04 23:34:19 -0300

    Example API scripts added

---
+++ Services/API/APIHelper.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Net;
+using System.IO;
+
+/// <summary>
+/// Example API Get call
+/// </summary>
+public static class APIHelper
+{
+    public static Joke GetNewJoke()
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.chucknorris.io/jokes/random");
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string json = reader.ReadToEnd();
+
+
+        return JsonUtility.FromJson<Joke>(json);
+    }
+}
---
+++ Services/API/Joke.cs
@@ -0,0 +1,8 @@
+/// <summary>
+/// Example class to serealize a web response
+/// </summary>
+[System.Serializable]
+public class Joke
+{
+    public string value;
+}
