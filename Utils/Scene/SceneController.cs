ccfb91afecf69869f582517380689202120410b6
Manuel Francisco Alonso <manuelalonso89@gmail.com>
2022-07-11 00:21:13 -0300

    SceneController modified. PlayerPrefsController added

--- Utils/Scene/SceneController.cs
+++ Utils/Scene/SceneController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 /// <summary>
@@ -5,47 +8,75 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class SceneController : Singleton<SceneController>
 {
+    private void Start()
+    {
+        Restart(5);
+    }
+
     /// <summary>
     /// Load next scene Index
     /// </summary>
-    public void LoadNextScene()
+    public void LoadNextScene(float timeDelay = 0)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(WaitForTime(
+            timeDelay,
+            () => SceneManager.LoadScene(SceneManager.GetActiveScene().
+                buildIndex + 1)));
     }
 
     // Load scene by index
-    public void LoadSceneByIndex(int sceneIndex)
+    public void LoadSceneByIndex(int sceneIndex, float timeDelay = 0)
     {
-        SceneManager.LoadScene(sceneIndex);
+        StartCoroutine(WaitForTime(
+            timeDelay,
+            () => SceneManager.LoadScene(sceneIndex)));
     }
 
     // Load scene by name
-    public void LoadSceneByName(string sceneName)
+    public void LoadSceneByName(string sceneName, float timeDelay = 0)
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(WaitForTime(
+            timeDelay,
+            () => SceneManager.LoadScene(sceneName)));
     }
 
     /// <summary>
     /// Load scene with 0 index
     /// </summary>
-    public void Restart()
+    public void Restart(float timeDelay = 0)
     {
-        SceneManager.LoadScene(0);
+        StartCoroutine(WaitForTime(
+            timeDelay,
+            () => SceneManager.LoadScene(0)));
     }
 
     // Reload scene
-    public void ReloadScene()
+    public void ReloadScene(float timeDelay = 0)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        StartCoroutine(WaitForTime(
+            timeDelay,
+            () => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex)));
     }
 
     // Quit
-    public void Quit()
+    public void Quit(float timeDelay = 0)
     {
+        StartCoroutine(WaitForTime(
+            timeDelay,
+            () =>
+            {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.ExitPlaymode();
+                UnityEditor.EditorApplication.ExitPlaymode();
 #else
-		Application.Quit();
+		        Application.Quit();
 #endif
+            }));
+    }
+
+    // Wait for time Coroutine
+    private IEnumerator WaitForTime(float timeDelay, Action callback)
+    {
+        yield return new WaitForSeconds(timeDelay);
+        callback();
     }
 }
