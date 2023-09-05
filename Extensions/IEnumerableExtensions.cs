fe1fad81f49fe1e6f4e02c93e01f06a192fca56d
manuelfalonso <manuelalonso89@gmail.com>
2023-09-04 23:19:01 -0300

    Moved DelegateEventExample.cs. Added IEnumerableExtensions.cs and MonoBehaviourExtensions.cs

---
+++ Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SombraStudios.Extensions
+{
+    /// <summary>
+    /// Extension methods for IEnumerables
+    /// </summary>
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> GetUniqueRandomItems<T>(this IEnumerable<T> items, int count)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentException("Count must be greater than 0");
+            }
+
+            var collection = items.ToList();
+            var rng = new System.Random(DateTime.Now.Millisecond);
+
+            if (count > collection.Count)
+            {
+                count = collection.Count;
+            }
+
+            for (var i = 0; i < count; i++)
+            {
+                var index = rng.Next(collection.Count);
+                var item = collection[index];
+                yield return item;
+                collection.RemoveAt(index);
+            }
+        }
+    }
+}
---
+++ Extensions/MonoBehaviourExtensions.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace SombraStudios.Extensions
+{
+    /// <summary>
+    /// Extension methods for MonoBehaviour classes
+    /// </summary>
+    public static class MonoBehaviourExtensions
+    {
+        public static void SafeInit<T>(this MonoBehaviour source, ref T component) where T : Component
+        {
+            if (component == null)
+            {
+                if (!source.TryGetComponent(out component))
+                {
+                    Debug.LogError("Reference variable not assigned and no component found.");
+                }
+            }
+        }
+    }
+}
