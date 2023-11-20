71bbebc9c3e0d0f38ce83c51c8a92d8ffcedb467
manuelfalonso <manuelalonso89@gmail.com>
2023-11-19 21:21:12 -0300

    Added UI mobile SafeArea.cs

---
+++ UI/Mobile/SafeArea.cs
@@ -0,0 +1,50 @@
+using SombraStudios.Extensions;
+using UnityEngine;
+
+namespace SombraStudios.UI.Mobile
+{
+    /// <summary>
+    /// Adjusts the RectTransform based on the device's safe area.
+    /// </summary>
+    public class SafeArea : MonoBehaviour
+    {
+        /// <summary>
+        /// Reference to the RectTransform that will be adjusted based on the safe area.
+        /// </summary>
+        [Tooltip("Reference to the RectTransform that will be adjusted based on the safe area.")]
+        [SerializeField] private RectTransform _rectTransform;
+
+
+        /// <summary>
+        /// Initializes the SafeArea and applies adjustments.
+        /// </summary>
+        private void Start()
+        {
+            // Ensure _rectTransform is initialized before applying the safe area
+            this.SafeInit(ref _rectTransform);
+            ApplySafeArea();
+        }
+
+
+        /// <summary>
+        /// Applies the device's safe area to the RectTransform.
+        /// </summary>
+        private void ApplySafeArea()
+        {
+            // Get the device's safe area
+            var safeArea = Screen.safeArea;
+
+            // Convert safe area values to canvas space
+            var minAnchor = safeArea.position;
+            var maxAnchor = safeArea.position * safeArea.size;
+            minAnchor.x /= Screen.width;
+            minAnchor.y /= Screen.height;
+            maxAnchor.x /= Screen.width;
+            maxAnchor.y /= Screen.height;
+
+            // Apply the safe area to the RectTransform
+            _rectTransform.anchorMin = minAnchor;
+            _rectTransform.anchorMax = maxAnchor;
+        }
+    }
+}
