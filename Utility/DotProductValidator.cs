31214af0c9a6fc369f5b79663728a2a8fac688ee
manuelfalonso <manuelalonso89@gmail.com>
2024-03-03 20:32:23 -0300

    Added xml summaries and modified behaviour to DotProductValidator.cs

--- Utility/DotProductValidator.cs
+++ Utility/DotProductValidator.cs
@@ -12,23 +12,74 @@ namespace SombraStudios.Shared.Utility
     public class DotProductValidator : MonoBehaviour
     {
         [Header("Vectors")]
+        /// <summary>
+        /// The first transform for dot product calculation.
+        /// </summary>
+        [Tooltip("The first transform for dot product calculation.")]
         [SerializeField] private Transform _transformOne;
+        /// <summary>
+        /// The direction for the first transform.
+        /// </summary>
+        [Tooltip("The direction for the first transform.")]
         [SerializeField] private Direction _directionOne;
+        /// <summary>
+        /// Flag to determine whether to compare with a transform or not.
+        /// </summary>
+        [Tooltip("Flag to determine whether to compare with a transform or not.")]
+        [SerializeField] private bool _compareWithTransform = true;
+        /// <summary>
+        /// The second transform for dot product calculation.
+        /// </summary>
+        [Tooltip("The second transform for dot product calculation.")]
         [SerializeField] private Transform _transformTwo;
+        /// <summary>
+        /// The direction for the second transform.
+        /// </summary>
+        [Tooltip("The direction for the second transform.")]
         [SerializeField] private Direction _directionTwo;
 
         [Header("Config")]
+        /// <summary>
+        /// Flag to determine if dot product validation should occur automatically.
+        /// </summary>
+        [Tooltip("Flag to determine if dot product validation should occur automatically.")]
         [SerializeField] private bool _automaticValidate = true;
+        /// <summary>
+        /// Time in seconds to validate dot product
+        /// </summary>
         [Tooltip("Time in seconds to validate dot product")]
         [SerializeField] private float _tickTime = 0.1f;
 
         [Header("Debug")]
+        /// <summary>
+        /// The current dot product value.
+        /// </summary>
+        [Tooltip("The current dot product value.")]
         [SerializeField, ReadOnly] protected float _currentDotProduct = 0f;
+        /// <summary>
+        /// Flag to show debug logs.
+        /// </summary>
+        [Tooltip("Flag to show debug logs.")]
         [SerializeField] private bool _showLogs;
+        /// <summary>
+        /// Flag to show gizmos.
+        /// </summary>
+        [Tooltip("Flag to show gizmos.")]
         [SerializeField] private bool _showGizmo;
+        /// <summary>
+        /// The gizmo utility for the first direction.
+        /// </summary>
+        [Tooltip("The gizmo utility for the first direction.")]
         [SerializeField] private GizmosLineUtility _directionOneGizmo;
+        /// <summary>
+        /// The gizmo utility for the second direction.
+        /// </summary>
+        [Tooltip("The gizmo utility for the second direction.")]
         [SerializeField] private GizmosLineUtility _directionTwoGizmo;
 
+        /// <summary>
+        /// The current dot product value.
+        /// </summary>
         public float CurrentDotProduct
         {
             get
@@ -43,6 +94,9 @@ namespace SombraStudios.Shared.Utility
             }
         }
 
+        /// <summary>
+        /// Event invoked when the dot product changes.
+        /// </summary>
         public UnityEvent<float> DotProductChanged;
 
         private WaitForSeconds _tickDelay;
@@ -65,24 +119,36 @@ namespace SombraStudios.Shared.Utility
         }
 
 
+        /// <summary>
+        /// Calculates the dot product.
+        /// </summary>
         public virtual float CalculateDotProduct()
         {
             var dotProduct = 0f;
 
-            var vectorOneDirection = GetVectorFromDirection((Direction)_directionOne);
+            var vectorOneDirection = GetVectorFromDirection(_directionOne);
             if (vectorOneDirection == Vector3.zero) { return dotProduct; }
             var vectorOne = _transformOne.TransformDirection(vectorOneDirection);
             Tools.Logger.Log(_showLogs, $"Vector One: {vectorOne}", this);
 
-            var vectorTwoDirection = GetVectorFromDirection((Direction)_directionTwo);
+            var vectorTwoDirection = GetVectorFromDirection(_directionTwo);
             if (vectorTwoDirection == Vector3.zero) { return dotProduct; }
-            var vectorTwo = _transformTwo.TransformDirection(vectorTwoDirection);
+            var vectorTwo = Vector3.zero;
+            if (_compareWithTransform)
+            {
+                vectorTwo = _transformTwo.TransformDirection(vectorTwoDirection);
+            }
+            else
+            {
+                vectorTwo = GetVectorFromDirection(_directionTwo);
+            }
             Tools.Logger.Log(_showLogs, $"Vector Two: {vectorTwo}", this);
 
             SetLineGizmos(_directionOneGizmo, vectorOne);
             SetLineGizmos(_directionTwoGizmo, vectorTwo);
 
-            CurrentDotProduct = Mathf.Round(Vector3.Dot(vectorOne, vectorTwo) * 100f) / 100f;
+            //CurrentDotProduct = Mathf.Round(Vector3.Dot(vectorOne, vectorTwo) * 100f) / 100f;
+            CurrentDotProduct = Vector3.Dot(vectorOne, vectorTwo);
             Tools.Logger.Log(_showLogs, $"Current Dot Product: {_currentDotProduct}", this);
 
             dotProduct = CurrentDotProduct;
@@ -90,6 +156,9 @@ namespace SombraStudios.Shared.Utility
         }
 
 
+        /// <summary>
+        /// Runs continuous dot product calculations.
+        /// </summary>
         private IEnumerator RunTicks()
         {
             if (!_automaticValidate) { yield break; }
@@ -101,27 +170,26 @@ namespace SombraStudios.Shared.Utility
             }
         }
 
+        /// <summary>
+        /// Retrieves the vector from a specified direction.
+        /// </summary>
         private Vector3 GetVectorFromDirection(Direction direction)
         {
-            switch (direction)
+            return direction switch
             {
-                case Direction.Up:
-                    return Vector3.up;
-                case Direction.Down:
-                    return Vector3.down;
-                case Direction.Left:
-                    return Vector3.left;
-                case Direction.Right:
-                    return Vector3.right;
-                case Direction.Foward:
-                    return Vector3.forward;
-                case Direction.Back:
-                    return Vector3.back;
-                default:
-                    return Vector3.zero;
-            }
+                Direction.Up => Vector3.up,
+                Direction.Down => Vector3.down,
+                Direction.Left => Vector3.left,
+                Direction.Right => Vector3.right,
+                Direction.Foward => Vector3.forward,
+                Direction.Back => Vector3.back,
+                _ => Vector3.zero,
+            };
         }
 
+        /// <summary>
+        /// Sets the gizmo lines.
+        /// </summary>
         private void SetLineGizmos(GizmosLineUtility line, Vector3 toDirection)
         {
             if (!_showGizmo) { return; }
@@ -130,6 +198,9 @@ namespace SombraStudios.Shared.Utility
         }
     }
 
+    /// <summary>
+    /// Enumeration of possible Vector3 directions.
+    /// </summary>
     public enum Direction
     {
         Up = 1,
