2dea9c272bb4bd976ec1b2b135e0be73099c9197
manuelfalonso <manuelalonso89@gmail.com>
2024-05-22 23:48:07 -0300

    Added OnEnter and OnExit animations state event triggers.

---
+++ Animations/AnimationEventActionBegin.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace SombraStudios.Shared.Animation
+{
+    /// <summary>
+    /// Enables a component to react to the 'ActionBegin' animation event.
+    /// </summary>
+    /// <seealso cref="IAnimationEventActionFinished"/>
+    public interface IAnimationEventActionBegin
+    {
+        /// <summary>
+        /// Called when the target animation begins.
+        /// </summary>
+        /// <param name="label">A label identifying the animation that has began</param>
+        void ActionBegin(string label);
+    }
+
+    /// <summary>
+    /// Calls the 'ActionBegin' function on any supported component when the target animation begins.
+    /// </summary>
+    /// <seealso cref="AnimationEventActionFinished"/>
+    public class AnimationEventActionBegin : StateMachineBehaviour
+    {
+        [SerializeField]
+        [Tooltip("A label identifying the animation that has started.")]
+        private string _label;
+
+        /// <inheritdoc />
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            var eventReceiver = animator.GetComponentInParent<IAnimationEventActionBegin>();
+            eventReceiver?.ActionBegin(_label);
+        }
+    }
+}
---
+++ Animations/AnimationEventActionFinished.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace SombraStudios.Shared.Animation
+{
+    /// <summary>
+    /// Enables a component to react to the 'ActionFinished' animation event.
+    /// </summary>
+    /// <seealso cref="IAnimationEventActionBegin"/>
+    public interface IAnimationEventActionFinished
+    {
+        /// <summary>
+        /// Called when the target animation exits.
+        /// </summary>
+        /// <param name="label">A label identifying the animation that has finished</param>
+        void ActionFinished(string label);
+    }
+
+    /// <summary>
+    /// Calls the 'ActionFinished' function on any supported component when the target animation exits.
+    /// </summary>
+    /// <seealso cref="AnimationEventActionBegin"/>
+    public class AnimationEventActionFinished : StateMachineBehaviour
+    {
+        [SerializeField]
+        [Tooltip("A label identifying the animation that has finished.")]
+        private string _label;
+
+        /// <inheritdoc />
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            var eventReceiver = animator.GetComponentInParent<IAnimationEventActionFinished>();
+            eventReceiver?.ActionFinished(_label);
+        }
+    }
+}
