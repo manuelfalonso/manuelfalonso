71aa8fa6266f86f8bf6cf5eda9fbb8aa7650368a
manuelfalonso <manuelalonso89@gmail.com>
2024-11-17 14:02:36 -0300

    Added CommandScriptableObject with generic type

--- Patterns/Behavioural/Command/CommandScriptableObject.cs
+++ Patterns/Behavioural/Command/CommandScriptableObject.cs
@@ -29,4 +29,36 @@ namespace SombraStudios.Shared.Patterns.Behavioural.Command
         /// </summary>
         protected abstract void Execute();
     }
+
+    /// <summary>
+    /// Abstract base class for command pattern implementation using ScriptableObject.
+    /// </summary>
+    /// <typeparam name="T">The type of data that the command operates on.</typeparam>
+    public abstract class CommandScriptableObject<T> : ScriptableObject where T : struct
+    {
+        /// <summary>
+        /// Tries to execute the command if it can be executed.
+        /// </summary>
+        /// <param name="data">The data that the command operates on.</param>
+        public void TryExecute(T? data = null)
+        {
+            if (CanExecute(data))
+            {
+                Execute(data);
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the command can be executed.
+        /// </summary>
+        /// <param name="data">The data that the command operates on.</param>
+        /// <returns>True if the command can be executed, otherwise false.</returns>
+        protected abstract bool CanExecute(T? data = null);
+
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        /// <param name="data">The data that the command operates on.</param>
+        protected abstract void Execute(T? data = null);
+    }
 }
