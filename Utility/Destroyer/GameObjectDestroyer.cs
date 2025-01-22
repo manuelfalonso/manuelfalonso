133a0f5b839b6b9968baf540fcf78137fbdad38f
manuelfalonso <manuelalonso89@gmail.com>
2025-01-22 12:31:59 -0300

    Added GameObjectDestroyer.cs and ScriptDestroyer.cs

---
+++ Utility/Destroyer/GameObjectDestroyer.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+
+namespace SombraStudios.Shared.Utility.Destroyer
+{
+    /// <summary>
+    /// Destroys on Awake the GameObject this script is attached to depending on platform scripting symbols like
+    /// UNITY_EDITOR, UNITY_STANDALONE, UNITY_ANDROID, etc.
+    /// </summary>
+    public class GameObjectDestroyer : MonoBehaviour
+    {
+        [Header("Platform selection")]
+        [Tooltip("Destroy the GameObject if running in the Unity editor.")]
+        [SerializeField]
+        private bool _destroyInEditor = false;
+
+        [Tooltip("Destroy the GameObject if running on a standalone platform (Mac OS X, Windows or Linux).")]
+        [SerializeField]
+        private bool _destroyOnStandalone = false;
+
+        [Tooltip("Destroy the GameObject if running on an Android platform.")] [SerializeField]
+        private bool _destroyOnAndroid = false;
+
+        [Tooltip("Destroy the GameObject if running on an iOS platform.")] [SerializeField]
+        private bool _destroyOnIOS = false;
+
+        [Tooltip("Destroy the GameObject if running on a WebGL platform.")] [SerializeField]
+        private bool _destroyOnWebGL = false;
+
+
+        [Header("Others")] [Tooltip("Destroy the GameObject if running on a development build.")] [SerializeField]
+        private bool _destroyOnDevelopmentBuild = false;
+
+
+        private void Awake()
+        {
+            if (ShouldDestroy())
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private bool ShouldDestroy()
+        {
+#if DEVELOPMENT_BUILD
+            if (_destroyOnDevelopmentBuild)
+            {
+                return true;
+            }
+#endif
+
+#if UNITY_EDITOR
+            if (_destroyInEditor)
+            {
+                return true;
+            }
+#endif
+
+#if UNITY_STANDALONE
+            if (_destroyOnStandalone)
+            {
+                return true;
+            }
+#endif
+
+#if UNITY_ANDROID
+            if (_destroyOnAndroid)
+            {
+                return true;
+            }
+#endif
+
+#if UNITY_IOS
+            if (_destroyOnIOS)
+            {
+                return true;
+            }
+#endif
+
+#if UNITY_WEBGL
+            if (_destroyOnWebGL)
+            {
+                return true;
+            }
+#endif
+
+            return false;
+        }
+    }
+}
---
+++ Utility/Destroyer/ScriptDestroyer.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+
+namespace SombraStudios.Shared.Utility
+{
+    /// <summary>
+    /// Destroys on Awake the scripts specified depending on platform scripting symbols like
+    /// UNITY_EDITOR, UNITY_STANDALONE, UNITY_ANDROID, etc.
+    /// </summary>
+    public class ScriptDestroyer : MonoBehaviour
+    {
+        [Tooltip("An array of script components to be destroyed.")]
+        [SerializeField] private Component[] _scriptsToDestroy; 
+        
+        [Header("Platform selection")]
+        [Tooltip("Destroy the GameObject if running in the Unity editor.")]
+        [SerializeField]
+        private bool _destroyInEditor = false;
+
+        [Tooltip("Destroy the GameObject if running on a standalone platform (Mac OS X, Windows or Linux).")]
+        [SerializeField]
+        private bool _destroyOnStandalone = false;
+
+        [Tooltip("Destroy the GameObject if running on an Android platform.")] [SerializeField]
+        private bool _destroyOnAndroid = false;
+
+        [Tooltip("Destroy the GameObject if running on an iOS platform.")] [SerializeField]
+        private bool _destroyOnIOS = false;
+
+        [Tooltip("Destroy the GameObject if running on a WebGL platform.")] [SerializeField]
+        private bool _destroyOnWebGL = false;
+
+        [Header("Others")] [Tooltip("Destroy the GameObject if running on a development build.")] [SerializeField]
+        private bool _destroyOnDevelopmentBuild = false;
+        
+        
+        private void Awake()
+        {
+            if (ShouldDestroy())
+            {
+                foreach (var item in _scriptsToDestroy)
+                {
+                    if (item != null)
+                    {
+                        Destroy(item);
+                    }
+                }
+            }
+        }
+        
+        private bool ShouldDestroy()
+        {
+#if DEVELOPMENT_BUILD
+            if (_destroyOnDevelopmentBuild)
+            {
+                return true;
+            }
+#endif
+
+#if UNITY_EDITOR
+            if (_destroyInEditor)
+            {
+                return true;
+            }
+#endif
+
+#if UNITY_STANDALONE
+            if (_destroyOnStandalone)
+            {
+                return true;
+            }
+#endif
+
+#if UNITY_ANDROID
+            if (_destroyOnAndroid)
+            {
+                return true;
+            }
+#endif
+
+#if UNITY_IOS
+            if (_destroyOnIOS)
+            {
+                return true;
+            }
+#endif
+
+#if UNITY_WEBGL
+            if (_destroyOnWebGL)
+            {
+                return true;
+            }
+#endif
+
+            return false;
+        }
+    }
+}
